d0ac0a2fbd9d40f6c378375aa6f2b7032239126f
nowang6 <norman6.wang@gmail.com>
2025-06-20 18:07:17 +0800

    fix: fix docker load

--- download_images.sh
+++ download_images.sh
@@ -20,14 +20,14 @@ esac
 echo "Detected system architecture: $ARCH, using platform: $PLATFORM"
 
 # 登录华为云镜像仓库
-docker login -u cn-south-1@HST3UZHVUG3KOJ367GOI swr.cn-south-1.myhuaweicloud.com
+#docker login -u cn-south-1@HST3UZHVUG3KOJ367GOI swr.cn-south-1.myhuaweicloud.com
 # 输入密码
 
 # 下载300I-Duo镜像
-docker pull --platform=$PLATFORM swr.cn-south-1.myhuaweicloud.com/ascendhub/mindie:1.0.0-300I-Duo-py311-openeuler24.03-lts
+#docker pull --platform=$PLATFORM swr.cn-south-1.myhuaweicloud.com/ascendhub/mindie:1.0.0-300I-Duo-py311-openeuler24.03-lts
 
 # 下载800I-A2镜像
-docker pull --platform=$PLATFORM swr.cn-south-1.myhuaweicloud.com/ascendhub/mindie:1.0.0-800I-A2-py311-openeuler24.03-lts
+# docker pull --platform=$PLATFORM swr.cn-south-1.myhuaweicloud.com/ascendhub/mindie:1.0.0-800I-A2-py311-openeuler24.03-lts
 
 # 保存镜像
 docker save -o images/mindie-1.0.0-300I-Duo-py311-openeuler24.03-lts.tar swr.cn-south-1.myhuaweicloud.com/ascendhub/mindie:1.0.0-300I-Duo-py311-openeuler24.03-lts
--- load_images.sh
+++ load_images.sh
@@ -7,12 +7,12 @@
 # 列出加载的镜像
 # docker images | grep emotion-recognition
 # 加载300I-Duo镜像
-docker load -i mindie-1.0.0-300I-Duo-py311-openeuler24.03-lts.tar
+docker load -i docker/images/mindie-1.0.0-300I-Duo-py311-openeuler24.03-lts.tar
 # 设置简短名称
 docker tag swr.cn-south-1.myhuaweicloud.com/ascendhub/mindie:1.0.0-300I-Duo-py311-openeuler24.03-lts mindie1.0:300i-duo
 
 # 加载800I-A2镜像
-docker load -i mindie-1.0.0-800I-A2-py311-openeuler24.03-lts.tar
+docker load -i docker/images/mindie-1.0.0-800I-A2-py311-openeuler24.03-lts.tar
 # 设置简短名称
 docker tag swr.cn-south-1.myhuaweicloud.com/ascendhub/mindie:1.0.0-800I-A2-py311-openeuler24.03-lts mindie1.0:800i-a2
 
